7cf2465cca9e7d2cfdb23306dc1e35a9fe9b7ca5
gchen <genhan.chen@azgs.az.gov>
2011-09-01 10:21:35 -0700

    Change the structure of the Program

--- CSWClient/FormCSW.cs
+++ CSWClient/FormCSW.cs
@@ -8,15 +8,6 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 
-using ESRI.ArcGIS.Framework;
-using ESRI.ArcGIS.Carto;
-using ESRI.ArcGIS.ArcMapUI;
-using ESRI.ArcGIS.GISClient;
-using ESRI.ArcGIS.esriSystem;
-using ESRI.ArcGIS.Display;
-using ESRI.ArcGIS.Geodatabase;
-using ESRI.ArcGIS.Desktop.AddIns;
-
 namespace ArcMapAddin1
 {
     public partial class FormCSW : Form
@@ -26,126 +17,16 @@ namespace ArcMapAddin1
             InitializeComponent();
         }
 
+        private ServiceOpener cSvcOpener = new ServiceOpener();
+        
         private void buttonAddWms_Click(object sender, EventArgs e)
-        {
-
-            try
-            {
-                ///Create an WMSMapLayer Instance - this will be added to the map later 
-                IWMSGroupLayer pWMSMapLayer = new WMSMapLayerClass() as IWMSGroupLayer;
-
-                ///Create and configure wms connection name, this is used to store the connection properties
-                IWMSConnectionName pConnName = new WMSConnectionNameClass();
-                IPropertySet pPropSet = new PropertySetClass();
-                pPropSet.SetProperty("URL", txtboxWmsUrl.Text);
-                pConnName.ConnectionProperties = pPropSet;
-
-                ///Use the name information to connect to the service
-                IDataLayer pDataLayer = pWMSMapLayer as IDataLayer;
-                IName pName = pConnName as IName;
-                pDataLayer.Connect(pName);
-
-                ///Get service description, which includes the categories of wms layers
-                IWMSServiceDescription pServiceDesc = pWMSMapLayer.WMSServiceDescription;
-
-                ///Configure the layer before adding it to the map
-                ILayer pLayer = pWMSMapLayer as ILayer;
-                pLayer.Name = pServiceDesc.WMSTitle;
-
-                ///Add layer to Map
-                IMxDocument pMxDoc = ArcMap.Document;
-                pMxDoc.FocusMap.AddLayer(pLayer);
-
-                ///Refresh
-                IActiveView pActiveView = pMxDoc.FocusMap as IActiveView;
-                pActiveView.Refresh();
-            }
-            catch (System.Runtime.InteropServices.COMException cEx)
-            {
-                ///Catch comexception handler
-                MessageBox.Show("The wms url is invalid!\r\rError message:\r" + cEx.Message);
-            }
-            catch (Exception ex)
-            { 
-                ///Catch general exception handler
-                MessageBox.Show(ex.StackTrace);
-            }
-            
-
-
+        {            
+            cSvcOpener.OpenWMS(txtboxWmsUrl.Text);
         }
 
         private void buttonAddMapSvr_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string strUrl = txtboxWmsUrl.Text;
-                string[] strUrlParts = strUrl.Split('/');
-                string strUrlSvr = null; 
-                string strSvrName = null;
-
-                for (int i = 0; i < strUrlParts.Length; i ++)
-                {
-                    if (strUrlParts[i] == "services")
-                    {
-                        strUrlSvr = string.Join("/", strUrlParts.Take(i + 1).ToArray());
-                        
-                        for (int j = i + 1; j < strUrlParts.Length - 1; j++)
-                        {
-                            if(strSvrName != null)
-                                strSvrName += "/";
-                            strSvrName += strUrlParts[j]; 
-                        }
-                        break;
-                    }
-                }
-
-
-                
-                IAGSServerConnectionFactory pAGSSvrConnFactory = new AGSServerConnectionFactoryClass();
-                IPropertySet pPropSet = new PropertySetClass();
-                pPropSet.SetProperty("URL", strUrlSvr);
-                IAGSServerConnection pAGSSvrConn = pAGSSvrConnFactory.Open(pPropSet, 0);
-                
-                IAGSEnumServerObjectName pEnumSOName = pAGSSvrConn.ServerObjectNames;
-                IAGSServerObjectName pAGSSOName = pEnumSOName.Next();
-
-                while (pAGSSOName != null)
-                {
-                    Debug.WriteLine(pAGSSOName.Name + ":" + pAGSSOName.Type);
-
-                    if (pAGSSOName.Name == strSvrName && pAGSSOName.Type == "MapServer")
-                    {
-                        break;
-                    }
-                    
-                    pAGSSOName = pEnumSOName.Next();
-                }
-
-                IName pName = pAGSSOName as IName;
-                IMapServer pMapServer = pName.Open() as IMapServer;
-
-                IMapServerLayer pMapSvrLayer = new MapServerLayerClass();
-                pMapSvrLayer.ServerConnect(pAGSSOName, pMapServer.DefaultMapName);
-
-                ILayer pLayer = pMapSvrLayer as ILayer;
-
-                ///Add layer to Map
-                IMxDocument pMxDoc = ArcMap.Document;
-                pMxDoc.FocusMap.AddLayer(pLayer);
-
-                ///Refresh
-                IActiveView pActiveView = pMxDoc.FocusMap as IActiveView;
-                pActiveView.Refresh();
-
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.StackTrace);
-            }
-
-
+            cSvcOpener.OpenAGS(txtboxWmsUrl.Text);
         }
     }
 }
---
+++ CSWClient/ServiceOpener.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.GISClient;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Desktop.AddIns;
+
+namespace ArcMapAddin1
+{
+    public class ServiceOpener
+    {
+    /// <summary>
+    /// Add a new layer to the current map
+    /// </summary>
+    /// <param name="pLayer"></param>
+        private void AddLayer(ILayer pLayer)
+        {
+            ///Add layer to Map
+            IMxDocument pMxDoc = ArcMap.Document;
+            pMxDoc.FocusMap.AddLayer(pLayer);
+
+            ///Refresh
+            IActiveView pActiveView = pMxDoc.FocusMap as IActiveView;
+            pActiveView.Refresh();
+        }
+
+    /// <summary>
+    /// Parse WMS Service
+    /// </summary>
+    /// <param name="urlWMS"></param>
+        public void OpenWMS(string urlWMS)
+        {
+            try
+            {
+                ///Create an WMSMapLayer Instance - this will be added to the map later 
+                IWMSGroupLayer pWMSMapLayer = new WMSMapLayerClass() as IWMSGroupLayer;
+
+                ///Create and configure wms connection name, this is used to store the connection properties
+                IWMSConnectionName pConnName = new WMSConnectionNameClass();
+                IPropertySet pPropSet = new PropertySetClass();
+                pPropSet.SetProperty("URL", urlWMS);
+                pConnName.ConnectionProperties = pPropSet;
+
+                ///Use the name information to connect to the service
+                IDataLayer pDataLayer = pWMSMapLayer as IDataLayer;
+                IName pName = pConnName as IName;
+                pDataLayer.Connect(pName);
+
+                ///Get service description, which includes the categories of wms layers
+                IWMSServiceDescription pServiceDesc = pWMSMapLayer.WMSServiceDescription;
+
+                ///Configure the layer before adding it to the map
+                ILayer pLayer = pWMSMapLayer as ILayer;
+                pLayer.Name = pServiceDesc.WMSTitle;
+                AddLayer(pLayer);
+            }
+            catch (System.Runtime.InteropServices.COMException cEx)
+            {
+                ///Catch comexception handler
+                MessageBox.Show("The wms url is invalid!\r\rError message:\r" + cEx.Message);
+            }
+            catch (Exception ex)
+            {
+                ///Catch general exception handler
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+    /// <summary>
+    /// Parse ArcGIS MapServer Service
+    /// </summary>
+    /// <param name="urlAGS"></param>
+        public void OpenAGS(string urlAGS)
+        {
+            try
+            {
+                ///Parse the url for MapServer -- get the service url and the service name 
+                string strUrl = urlAGS;
+                string[] strUrlParts = strUrl.Split('/');
+                string strUrlSvr = null;
+                string strSvrName = null;
+
+                for (int i = 0; i < strUrlParts.Length; i++)
+                {
+                    if (strUrlParts[i] == "services")
+                    {
+                        strUrlSvr = string.Join("/", strUrlParts.Take(i + 1).ToArray());
+
+                        for (int j = i + 1; j < strUrlParts.Length - 1; j++)
+                        {
+                            if (strSvrName != null)
+                                strSvrName += "/";
+                            strSvrName += strUrlParts[j];
+                        }
+                        break;
+                    }
+                }
+
+
+                ///Set up ArcGIS Service connection factory, which includes the list of services
+                IAGSServerConnectionFactory pAGSSvrConnFactory = new AGSServerConnectionFactoryClass();
+                IPropertySet pPropSet = new PropertySetClass();
+                pPropSet.SetProperty("URL", strUrlSvr);
+                IAGSServerConnection pAGSSvrConn = pAGSSvrConnFactory.Open(pPropSet, 0);
+
+                ///Get the list of services
+                IAGSEnumServerObjectName pEnumSOName = pAGSSvrConn.ServerObjectNames;
+                IAGSServerObjectName pAGSSOName = pEnumSOName.Next();
+
+                ///Get the specific service with the service name
+                while (pAGSSOName != null)
+                {
+                    Debug.WriteLine(pAGSSOName.Name + ":" + pAGSSOName.Type);
+
+                    if (pAGSSOName.Name == strSvrName && pAGSSOName.Type == "MapServer")
+                    {
+                        break;
+                    }
+
+                    pAGSSOName = pEnumSOName.Next();
+                }
+
+                ///Open the specific service
+                IName pName = pAGSSOName as IName;
+                IMapServer pMapServer = pName.Open() as IMapServer;
+
+                IMapServerLayer pMapSvrLayer = new MapServerLayerClass();
+                pMapSvrLayer.ServerConnect(pAGSSOName, pMapServer.DefaultMapName);
+
+                ///Configure the layer before adding it to the map
+                ILayer pLayer = pMapSvrLayer as ILayer;
+                AddLayer(pLayer);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+    }
+}
